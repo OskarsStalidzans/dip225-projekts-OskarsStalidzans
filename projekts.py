52e51baef18d70f6f3696cb03f1ac10211907de5
OskarsStalidzans <143953611+OskarsStalidzans@users.noreply.github.com>
2024-01-05 19:16:15 +0000

    commit

--- projekts.py
+++ projekts.py
@@ -1,7 +1,7 @@
 import PyPDF2
 import os
 
-result=0
+videjais_paterins=0
 
 nosaukumi=[]
 filenames=os.listdir("rekini/")
@@ -9,28 +9,32 @@ skaits=len(filenames)
 for i in range(0,skaits):
     filename= "rekini/" + filenames[i]
     nosaukumi.append(filename)
-print(nosaukumi)
+    nosaukumi.sort()
+# print(nosaukumi)
 
-row=[]
-for i in range(0, skaits):
+menesa_paterins=[]
+for i in range(0,skaits):
     pdf_file=PyPDF2.PdfReader(open(nosaukumi[i],"rb"))
     number_of_pages=len(pdf_file.pages)
     page1=pdf_file.pages[0]
     page2=pdf_file.pages[1]
     text1=page1.extract_text()
-    text2=page2.extract_text()
+    text2=page2.extract_text()    
     
-    if "INFORMĀCIJA PAR DABASGĀZES PATĒRIŅU" in text1:
-        vieta1 = text1.find("A003438301")
-        paterins = text1[vieta1+11:vieta1+15]
-        paterins = paterins.replace(" ", "")
-        row.append(paterins)
+    if i in range(0,skaits):
+        if "INFORMĀCIJA PAR DABASGĀZES PATĒRIŅU" in text1:
+            vieta1 = text1.find("  (A003438301")
+            paterins = text1[vieta1+14:vieta1+18]
+            paterins = paterins.replace(" ", "")
+            menesa_paterins.append(int(paterins))
+        else:
+            vieta2 = text2.find("  (A003438301")
+            paterins = text2[vieta2+14:vieta2+18]
+            paterins = paterins.replace(" ", "")
+            menesa_paterins.append(int(paterins))
 
-    # else:
-    #     vieta2 = text2.find("A003438301")
-    #     paterins = text2[vieta2+11:vieta2+15]
-    #     paterins = paterins.replace(" ", "")
-    #     row.append(paterins)
+videjais_paterins = (sum(menesa_paterins))/skaits
+videjais_paterins = round(videjais_paterins)
+print(videjais_paterins)
 
-print(row)
 exit()
